d147d3a3fde955955cbb9442d95791b9c9a49be4
Anoxmous <natnaelberhanu18@gmail.com>
2021-02-06 01:05:13 +0300

    login and signup pages ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:dalvic_lyrics_sharing_app/constants.dart';
+import 'package:dalvic_lyrics_sharing_app/screens/loginpage.dart';
+import 'package:dalvic_lyrics_sharing_app/screens/signuppage.dart';
 import 'package:dalvic_lyrics_sharing_app/screens/splashpage.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      color: Theme.of(context).primaryColor,
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Color(0xFF329D9C),
-        primarySwatch: Colors.black
+        primaryColor: kPrimary,
       ),
-      home: SplashPage()
+      home: LoginPage(
+      )
     );
   }
 }
--- lib/screens/signuppage.dart
+++ lib/screens/signuppage.dart
@@ -1,9 +1,131 @@
+import 'package:dalvic_lyrics_sharing_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class SignUpPage extends StatelessWidget{
+class SignUpPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white, //or set color with: Color(0xFF0000FF)
+    ));
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: Text(
+                    'Signup',
+                    style: TextStyle(
+                        color: kPrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                    child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Full Name',
+                          fillColor: kPrimaryLight,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: kPrimary)),
+                        ),
+                        validator: (value) {
+                          if(value.isEmpty){
+                            return 'Field Required';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          fillColor: kPrimaryLight,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: kPrimary)),
+                        ),
+                        validator: (value) {
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          fillColor: kPrimaryLight,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: kPrimary)),
+                        ),
+                        validator: (value) {
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Confirm Password',
+                          fillColor: kPrimaryLight,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: kPrimary)),
+                        ),
+                        validator: (value) {
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical:8.0),
+                      child: FlatButton(
+                        minWidth: double.infinity,
+                          height: 50.0,
+                          onPressed: () {
+                            _formKey.currentState.validate();
+                          },
+                          color: kPrimary,
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                          )),
+                    )
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
